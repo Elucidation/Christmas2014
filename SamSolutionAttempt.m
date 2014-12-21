108e17d8b217b273e4ed06d5516121116136315f
Elucidation <elucidation@gmail.com>
2014-12-21 13:13:16 -0800

    added skeleton for finding template in image, gitignore

--- SamSolutionAttempt.m
+++ SamSolutionAttempt.m
@@ -4,6 +4,12 @@ addpath('./EncodedOutput');
 
 codedImage = imread('coded_image.bmp');
 randomCharMap = imread('randomCharMap.bmp');
-%% See stuff
+%% Debug See stuff
 %imshow(codedImage);
 %imagesc(randomCharMap);
+
+
+% Find position of first template
+template = imread('template01.bmp');
+[r,c] = findTemplateInImage(codedImage, template);
+secret_letter = char( randomCharMap(r,c) + 96 )
