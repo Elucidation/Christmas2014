eefc50247615214a3bf97089ae8d4b9d7b8c2d23
Elucidation <elucidation@gmail.com>
2014-12-21 12:59:01 -0800

    Simple matlab file reading images

---
+++ SamSolutionAttempt.m
@@ -0,0 +1,9 @@
+% Attempts Lizzy's Christmas 2014 challenge
+%% Load stuff
+addpath('./EncodedOutput');
+
+codedImage = imread('coded_image.bmp');
+randomCharMap = imread('randomCharMap.bmp');
+%% See stuff
+%imshow(codedImage);
+%imagesc(randomCharMap);
