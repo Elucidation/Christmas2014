78bfd4e9893c40d9db2cb32584dcde987821f5e9
Elucidation <elucidation@gmail.com>
2014-12-21 13:34:13 -0800

    Finished

--- SamSolutionAttempt.m
+++ SamSolutionAttempt.m
@@ -4,12 +4,8 @@ addpath('./EncodedOutput');
 
 codedImage = imread('coded_image.bmp');
 randomCharMap = imread('randomCharMap.bmp');
-%% Debug See stuff
-%imshow(codedImage);
-%imagesc(randomCharMap);
 
-
-% Find position of first template
+%% Do stuff
 message = '';
 for i = 1:18
     % Load template
@@ -19,4 +15,8 @@ for i = 1:18
     % Map to randomCharMap and add character to message
     message = strcat(message, char(randomCharMap(r,c) + 96 ));
 end
+% Replace ` with spaces
+message = strrep(message,'`', ' '); 
+
+%% Display secret message
 fprintf('Secret message: %s\n', message);
